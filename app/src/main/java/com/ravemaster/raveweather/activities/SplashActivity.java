53ead728aba37899a2924224baa31f132acc6c6a
Moses_Ndungu_G_ <moisendungu@gmail.com>
2025-01-31 09:29:20 +0300

    Added click feature to each day's forecast. Added clear text mode on search edittext. Added splashscreen that works on all android versions, light and dark. App complete

--- app/src/main/java/com/ravemaster/raveweather/activities/SplashActivity.java
+++ app/src/main/java/com/ravemaster/raveweather/activities/SplashActivity.java
@@ -6,25 +6,22 @@ import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
 import androidx.core.splashscreen.SplashScreen;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.ravemaster.raveweather.R;
 
-public class TestAnimation extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         SplashScreen.installSplashScreen(this);
-        setContentView(R.layout.activity_test_animation);
+        setContentView(R.layout.activity_splash);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(TestAnimation.this, MainActivity.class));
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
                 finish();
             }
         },2000);
